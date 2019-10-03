731746ba49d7c1b3ce8cdae063f856507517564c
jules blanc <jules.blanc@unccub.com>
2019-10-03 12:37:10 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while counter < array.length
+    if array[counter] == value_to_find
+      return counter
+    end
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  return array.sort.uniq.last
 end
 
 def find_min_value(array)
-  # Add your solution here
+  return array.sort.uniq.first 
 end
